cd37e07494230a4c7f92de1595528e05a229575f
simon folkerts <simfolkerts@gmail.com>
2017-02-02 16:52:06 +1300

    Scrollbar prototype complete.

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,32 +1,70 @@
 $(document).ready(function() {
 
-    $('html, body').scrollTop(0).scrollTop(1).scrollTop(0);
-    //Add page markers that correspond to sections to the scrollbar
-    $(".section-container").each(function(index) {
-        $("#scroller").append('<p id="page-marker-' + index + '" class="page-marker">--</p>')
-    });
-
-    //Add event listeners to the page markers to scroll to element on click
-    var id;
-    $(".page-marker").click(function() {
-        id = $(this).attr('id').match(/\d+/g); //Get number from scroll marker id
-        $('html, body').stop().animate({
-            scrollTop: $("#section-" + id).position().top
-        }, 600);
-    });
-
-    $(window).scroll(function() {
+    function isSectionInView(id) {
         var currentScroll = $(document).scrollTop();
-        $(".section-container").each(function() {
-            if ($(this).offset().top < (currentScroll + $(this).outerHeight(true) / 2)) {
-                var id = $(this).attr('id').match(/\d+/g);
-                $('#page-marker-' + id).addClass('page-marker-current').html('0' + (parseInt(id) + 1));
+        var section = $("#section-" + id);
+        var sectionMiddle = $(section).outerHeight(true) / 2;
+        if (section.offset().top < (currentScroll + sectionMiddle) && (section.offset().top) > currentScroll - sectionMiddle) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function updateMarkers() {
+        $('.section-container').each(function() {
+            //Change classes based on the section currently in view
+            var id = $(this).attr('id').match(/\d+/g);
+            if (isSectionInView(id)) {
+                makeMarkerActive(id);
             } else {
-                $('#page-marker-' + id).removeClass('page-marker-current').html('--');
+                makeMarkerInactive(id);
             }
         });
-        console.log("\n\n\n");
+        updateEventListeners();
+    }
+
+    function updateEventListeners() {
+        //Remove all event listeners
+        $(".active").off();
+
+        //Reassign event listeners based on which markers are active or not
+        $(".inactive").on('mouseenter', function() {
+            var id = $(this).attr('id').match(/\d+/g);
+            $(this).html(parseInt(id) + 1);
+        });
+
+        $(".inactive").on('mouseleave', function() {
+            $(this).html('--');
+        });
+
+        //Add event listeners to inactive page markers to scroll to element on click
+        $(".inactive").click(function() {
+            var id = $(this).attr('id').match(/\d+/g);
+            $('html, body').stop().animate({
+                scrollTop: $("#section-" + id).position().top
+            }, 600);
+        });
+    }
+
+    function makeMarkerActive(id) {
+        $('#page-marker-' + id).removeClass('inactive').addClass('active').html(parseInt(id) + 1);
+    }
+
+    function makeMarkerInactive(id) {
+        $('#page-marker-' + id).removeClass('active').addClass('inactive').html('--');
+    }
+
+
+    //CONTROLLER
+
+    //Add page markers that correspond to sections to the scrollbar
+    $(".section-container").each(function(index) {
+        $("#scroller").append('<p id="page-marker-' + index + '" class="page-marker inactive">--</p>')
     });
 
 
+    updateMarkers();
+
+    $(window).scroll(updateMarkers);
 });
